c2e34ba0d87070fb6baee3d8d7eee7d4dd559101
Ayush Biju <ayushbiju8@gmail.com>
2024-06-19 22:33:35 +0530

    First

---
+++ Basics/first.js
@@ -0,0 +1,4 @@
+// Previously JS can only be written on browswee.
+// But now due to advancement in tech we can write JS on console.
+// To run JS we should install node js (or any others).
+console.log("Ayush");
